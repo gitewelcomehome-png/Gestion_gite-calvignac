5b5cc9bc1e4e6bdda13a44070f4ca20c442da477
gitewelcomehome-png <gite.welcomehome@gmail.com>
2025-12-29 16:22:58 +0000

    🐛 fix: Correction syntaxe JavaScript - déclaration fonction dupliquée

---
+++ clear_cleaning.sql
@@ -0,0 +1,5 @@
+-- Vider la table cleaning_schedule pour forcer le recalcul
+DELETE FROM cleaning_schedule;
+
+-- Afficher le résultat
+SELECT 'Table cleaning_schedule vidée avec succès' AS message;
