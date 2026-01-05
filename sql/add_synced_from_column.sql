e6b2c9d60702c213ac56da71c20d222d91fdc953
gitewelcomehome-png <gite.welcomehome@gmail.com>
2026-01-05 20:59:09 +0000

    Fix: Correction nom colonne 'plateforme' au lieu de 'site'

--- sql/add_synced_from_column.sql
+++ sql/add_synced_from_column.sql
@@ -9,21 +9,21 @@ ADD COLUMN IF NOT EXISTS synced_from TEXT;
 -- Commentaire
 COMMENT ON COLUMN reservations.synced_from IS 'Source de synchronisation iCal (Airbnb, Abritel, Gîtes de France, etc.)';
 
--- Mise à jour des données existantes en fonction du champ 'site'
+-- Mise à jour des données existantes en fonction du champ 'plateforme'
 UPDATE reservations 
 SET synced_from = 'Airbnb'
 WHERE synced_from IS NULL 
-  AND (site ILIKE '%airbnb%');
+  AND (plateforme ILIKE '%airbnb%');
 
 UPDATE reservations 
 SET synced_from = 'Abritel'
 WHERE synced_from IS NULL 
-  AND (site ILIKE '%abritel%' OR site ILIKE '%homelidays%');
+  AND (plateforme ILIKE '%abritel%' OR plateforme ILIKE '%homelidays%');
 
 UPDATE reservations 
 SET synced_from = 'Gîtes de France (centrale)'
 WHERE synced_from IS NULL 
-  AND site ILIKE '%gîtes de france%';
+  AND plateforme ILIKE '%gîtes de france%';
 
 -- Créer un index pour optimiser les requêtes
 CREATE INDEX IF NOT EXISTS idx_reservations_synced_from ON reservations(synced_from);
